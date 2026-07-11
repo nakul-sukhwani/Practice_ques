6ed166fc1e7a9910e0c27aa7dc2d5ab7678fdb16
Nakul Sukhwani <nakulsukhwani.in@gmail.com>
2026-07-11 22:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.04%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, j = 0;
+        while(i<n){
+            if(nums[i]==0){
+                i++;
+            }
+            else {
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+        }
+    }
+};
