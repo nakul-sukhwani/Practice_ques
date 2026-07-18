5f86b55e73ffa547fcabda662593958049761444
Nakul Sukhwani <nakulsukhwani.in@gmail.com>
2026-07-19 01:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (24.22%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,15 +3,10 @@ public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int ans = 0; 
-        for(int i = 0 ; i < nums.size()-1 ; i = i+2){
-           
-            if(nums[i] != nums[i+1]){
-                return nums[i];
-            }
-           
+        for(int i = 0 ; i < nums.size() ; i++){
+            ans ^= nums[i];
         }
-        
-    return nums[nums.size() - 1];// last element is unique if single element exists 
-    }
+        return ans; 
+        }
     
 };
